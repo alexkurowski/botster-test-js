18649c62ae67633dde3509a3b77e44c39d767827
Alex <kurowski.dev@gmail.com>
2022-01-15 11:06:20 +0300

    Update

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ import fs from "fs";
 (function () {
   const input = JSON.parse(fs.readFileSync("input.json"));
 
+  console.log("Log test")
+
   const result = [
     {
       'Message': "Hello, World!",
       "Got input": JSON.stringify(input)
     }
   ];
+
+  throw new Error("Error test")
 
   fs.writeFileSync("output.json", JSON.stringify(result));
 })();
